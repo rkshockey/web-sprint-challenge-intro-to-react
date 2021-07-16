1d5eab5d7c7a1cd383db1a168d0aac960afa7df9
rkshockey <rkshockey@gmail.com>
2021-07-16 12:45:30 -0400

    Implemented most of the MVP including character cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios'
+import { useState, useEffect } from 'react';
+import Character from './components/Character'
 
 const App = () => {
+  const [characters, setCharacters] = useState([])
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/people`)
+      .then(res => setCharacters(res.data))
+      .catch(err => console.log(err));
+  }, [])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -12,6 +22,7 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {characters.map(char => <Character character={char} />)}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,49 @@
-// Write your Character component here
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+function Character (props) {
+    const [show, setShow] = useState(false)
+    const { character } = props;
+
+    const StyledHead = styled.div`
+        display: flex;
+        justify-content: space-between;
+        margin: 0 20%;
+        background: #E9EB9E;
+        padding: 2%;
+    `
+    
+    const StyledButton = styled.button`
+        display: ${props => props.on ? show ? `none` : `inline` : show ? `inline` : `none`}
+    `
+
+    const StyledInfo = styled.div`
+        text-weight: bold;
+        color: #CCD9BF;
+        background: #A1867F;
+        margin: 0 20%;
+        padding: 2%;
+        display: ${show ? `block` : `none`}
+    `
+
+    return (
+        <div className='character'>
+            <StyledHead>
+                <h2>{character.name}</h2>
+                <StyledButton on={true} onClick={() => setShow(true)}>Show more</StyledButton>
+                <StyledButton on={false} onClick={() => setShow(false)}>Show Less</StyledButton>
+            </StyledHead>
+            <StyledInfo>
+                <p>Gender: {character.gender}</p>
+                <p>Height: {Math.floor(character.height / 2.54)}in</p>
+                <p>Mass: {Math.floor(character.mass * 2.2)}lb</p>
+                <p>Eye Color: {character.eye_color}</p>
+                <p>Hair Color: {character.hair_color}</p>
+                <p>Skin Tone: {character.skin_color}</p>
+                <p>Birth Year: {character.birth_year}</p>
+            </StyledInfo>
+        </div>
+    );
+};
+
+export default Character;
